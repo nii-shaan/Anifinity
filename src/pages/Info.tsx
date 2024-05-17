1d58c7b6406a1c79b3092ae5709a8dfb0263b949
Nishan Bista <nishanbista525@gmail.com>
2024-05-18 00:45:28 +0545

    feat:slug to page done

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
+import { GoDot } from "react-icons/go";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { MdOutlineSlideshow } from "react-icons/md";
 
 interface DataTypes {
   id: string;
@@ -56,6 +61,8 @@ function Info() {
   const [data, setData] = useState<DataTypes | null>(null);
   console.log(data);
 
+  const [desHidden, setDesHidden] = useState<boolean>(true);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -73,7 +80,86 @@ function Info() {
     fetchData();
   }, []);
 
-  return <div className="text-white">{slug}</div>;
+  return (
+    <div id="container" className="text-white">
+      <div className="flex flex-col items-center mt-8">
+        <div
+          id="poster"
+          className="w-48 h-64 bg-cover bg-no-repeat rounded-md"
+          style={{ backgroundImage: `url(${data?.image})` }}
+        ></div>
+        <div
+          id="title"
+          className={`w-[70%] text-center mt-5 ${
+            data == undefined
+              ? ""
+              : data?.title.length < 20
+              ? "text-2xl"
+              : data?.title.length < 30
+              ? "text-xl"
+              : "text-lg"
+          }`}
+        >
+          {data?.title}
+        </div>
+        <div
+          id="descriptionContainer"
+          className=" w-[80%] flex flex-col items-start mt-5 border p-6 border-[#2e2e2e] "
+        >
+          <div
+            id="description"
+            className={`text-sm w-full overflow-hidden text-[#909090] ${
+              !desHidden ? " h-auto" : "max-h-36"
+            }`}
+          >
+            {data?.description}
+          </div>
+          <div className="flex">
+            {desHidden ? (
+              <span
+                onClick={() => {
+                  setDesHidden(false);
+                }}
+                className="flex gap-2 items-center cursor-pointer"
+              >
+                More
+                <FaArrowRightLong className="w-5 h-5 mt-0.5" />
+              </span>
+            ) : (
+              <span
+                onClick={() => {
+                  setDesHidden(true);
+                }}
+                className="flex gap-2 items-center cursor-pointer"
+              >
+                <FaArrowLeftLong className="w-5 h-5 mt-0.5" />
+                Less
+              </span>
+            )}
+          </div>
+        </div>
+        <div id="infos">
+          <div>Total Episodes: {data?.totalEpisodes}</div>
+          <div>Sub: {data?.hasSub ? "Yes" : "No"}</div>
+          <div>Dub: {data?.hasDub ? "Yes" : "No"}</div>
+          <div className="flex ">
+            <GoDot className="w-4 h-6" />
+            {data?.type}
+          </div>
+        </div>
+        <div id="watch">
+          <Button
+            variant="outline"
+            className="w-full h-full bg-transparent px-28 py-2 flex justify-center items-center"
+          >
+            <Link to={`/`} className="flex items-center">
+              Watch <MdOutlineSlideshow className="h-6 w-6 ml-2" />
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
